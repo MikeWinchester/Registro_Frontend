36169fe5e4426d70d07e5a4ef920e285106c5274
Miguel Sánchez <aleminapavon30@gmail.com>
2025-03-16 05:34:02 -0600

    minor issues resolved

--- views/public/js/loginContoller.js
+++ views/public/js/loginContoller.js
@@ -2,25 +2,21 @@ import loadEnv from "./getEnv.mjs";
 const env = await loadEnv();
 
 document.querySelector(".btn-login").addEventListener("click", async () => {
-    // Obtener los valores de los campos de entrada
     const numeroCuenta = document.querySelector('input[placeholder="Número de Cuenta"]').value;
     const password = document.querySelector('input[placeholder="Contraseña"]').value;
 
-    // Validación simple
     if (!numeroCuenta || !password) {
         alert("Por favor ingresa ambos campos.");
         return;
     }
 
-    // Crear el cuerpo de la solicitud
     const loginData = {
         NumeroCuenta: numeroCuenta,
         Pass: password
     };
 
     try {
-        // Enviar la solicitud al endpoint de login
-        const response = await fetch(`${env.API_URL}/login`, {  // Cambia esta URL por la correcta
+        const response = await fetch(`${env.API_URL}/login`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -30,13 +26,10 @@ document.querySelector(".btn-login").addEventListener("click", async () => {
 
         const data = await response.json();
 
-        // Verificar la respuesta del servidor
         if (response.ok) {
-            // Guardar el JWT en el localStorage o manejar el token de acuerdo con tus necesidades
             localStorage.setItem("jwt", data.token);
-            window.location.href = "?page=estudiantes"; // Redirige a la página de inicio
+            window.location.href = "?page=estudiantes";
         } else {
-            // Mostrar mensaje de error
             alert(data.error || "Error desconocido.");
         }
     } catch (error) {
