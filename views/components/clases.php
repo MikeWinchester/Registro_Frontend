534e1538bd51bf6102a6adff23a20adc048150f0
Gerardo Rodriguez <55673748+g3rrarrd@users.noreply.github.com>
2025-03-17 19:15:48 -0600

    Add files via upload

--- views/components/clases.php
+++ views/components/clases.php
@@ -6,7 +6,7 @@
     <title>Mis Clases</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     
-    <link rel="stylesheet" href="/Registro_Frontend/assets/css/docentes/clases.css">
+    <link rel="stylesheet" href="../../assets/css/docentes/clases.css">
 </head>
 <body>
 
@@ -15,39 +15,26 @@
 <div class="container">
     <h2 class="mb-4"> Clases Asignadas</h2>
 
-    <table class="table table-striped table-hover">
+    <table id="tablaClases" class="table table-striped table-hover">
         <thead class="table-light">
             <tr>
                 <th>#</th>
                 <th>Materia</th>
-                <th>Grupo</th>
-                <th>Sección</th>
+                <th>Aula</th>
+                <th>Cupos</th>
+                <th>Horario</th>
                 <th>Estudiantes</th>
             </tr>
         </thead>
-        <tbody>
-            <?php
-                // Simulación de datos (debería venir de la BD)
-                $clases = [
-                    ["id" => 1, "materia" => "Matemáticas", "grupo" => "3A", "estudiantes" => 30],
-                    ["id" => 2, "materia" => "Historia", "grupo" => "2B", "estudiantes" => 25],
-                    ["id" => 3, "materia" => "Física", "grupo" => "1C", "estudiantes" => 28]
-                ];
-                
-                foreach ($clases as $index => $clase) {
-                    echo "<tr>
-                        <td>".($index + 1)."</td>
-                        <td>{$clase['materia']}</td>
-                        <td>{$clase['grupo']}</td>
-                        <td>{$clase['estudiantes']}</td>
-                        <td><a href='lista_estudiantes.php?clase_id={$clase['id']}' class='btn btn-info btn-sm'>Ver lista de estudiantes</a></td>
-                    </tr>";
-                }
-            ?>
+        <tbody id="tablaClasesBody">
+            <tr>
+                <td colspan="5" class="text-center">Cargando datos...</td>
+            </tr>
         </tbody>
     </table>
 </div>
 
+<script src="../public/js/seccionesDocentes.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
