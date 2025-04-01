e3aea2763aed9564f3b025dcf2ebbcf6aed8043b
g3rrarrd <g_rodriguez51@outlook.com>
2025-04-01 17:16:20 -0600

    modified:   assets/js/manejadorEstudiantes.js

--- assets/js/manejadorEstudiantes.js
+++ assets/js/manejadorEstudiantes.js
@@ -29,16 +29,23 @@ async function cargarEstudiantes() {
                                 <th>Estudiante</th>
                                 <th>Número de Cuenta</th>
                                 <th>Calificación</th>
+                                <th> Observacion </th>
                             </tr>
                         </thead>
                         <tbody>`;
 
+
+        let obs = await obtenerObservaciones();
         result.data.forEach(est => {
             console.log(est);
             html += `<tr>
                         <td>${est.nombre_completo}</td>
                         <td>${est.numero_cuenta}</td>
                         <td><input id='${est.estudiante_id}' type="number" class="form-control notas" min="0" max="20" id="nota_${est.estudiante_id}"></td>
+                        <td><select id='observacion'>
+                        <option value ="" disabled selected >OBS</option>
+                        ${obs}
+                        </select></td>
                      </tr>`;
         });
 
@@ -50,8 +57,30 @@ async function cargarEstudiantes() {
     }
 }
 
+async function obtenerObservaciones(){
+    try {
+        let response = await fetch(`${env.API_URL}/observacion/get`, {
+            method : "GET",
+        });
+
+        let result = await response.json(); 
+
+        let html = '';
+
+        result.data.forEach(obs => {
+            
+            html += `<option value=${obs.observacion_id}>${obs.observacion}</option>`;
+        });
+
+        return html;
+    } catch (error) {
+        console.error("Error al obtener estudiantes:", error);
+    }
+}
+
 async function guardarNotas(){
     const notas = document.querySelectorAll('.notas');
+    const obs = document.querySelector('#observacion');
 
     let clase = document.getElementById("claseSeleccionada").value;
     let est = {};
@@ -60,7 +89,7 @@ async function guardarNotas(){
     notas.forEach(nota => {
         num += 1;
         if(nota.value){
-           est[`Estudiante${num}`] = {'estudiante_id' : nota.id , 'seccion_id' : clase, 'nota': nota.value};
+           est[`Estudiante${num}`] = {'estudiante_id' : nota.id , 'seccion_id' : clase, 'nota': nota.value , 'observacion_id' : obs.value};
         }
     });
 
