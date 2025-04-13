ea519a4cbeeb8c30f8db16befeae6a6547debc4c
Miguel Sánchez <aleminapavon30@gmail.com>
2025-04-13 08:49:14 -0600

    staged changes

--- components/navbar.php
+++ components/navbar.php
@@ -14,7 +14,7 @@
                     <a class="nav-link" href="/">Inicio</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/matricula_estudiante/home">Matrícula</a>
+                    <a class="nav-link" href="/matricula/login/index.php">Matrícula</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="/library/login/index.php">Biblioteca Virtual</a>
